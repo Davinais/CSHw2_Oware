947e552cb3ba8a949d243a9788e7f44bb664416d
Davinais <david870620@gmail.com>
2016-11-03 02:44:05 +0800

    Add basic HousesMove method in GUI

--- src/OwareBoardGUI.java
+++ src/OwareBoardGUI.java
@@ -23,6 +23,7 @@ class HousesView extends ImageView
     {
         this.boardSide = boardSide;
         this.boardNum = boardNum;
+        //取得旗子圖片之寬與高
         double chessWidth = chessImage.getWidth() / cellsColumns;
         double chessHeight = chessImage.getHeight() / rowCounts;
         clips = new Rectangle2D[chessAmount];
@@ -31,7 +32,9 @@ class HousesView extends ImageView
             clips[i] = new Rectangle2D((i%cellsColumns)*chessWidth, (i/cellsColumns)*chessHeight, chessWidth, chessHeight);
         //讓滑鼠滑進去時有陰影
         DropShadow drop = new DropShadow(12, Color.GOLD);
-        //建立滑鼠滑入與滑出時的事件
+        //建立當點下滑鼠時會送出的事件，用以將起始位置傳送給BoardPane
+        HousesMoveEvent move = new HousesMoveEvent(getBoardPosition());
+        //設定滑鼠滑入與滑出時的事件
         setOnMouseEntered(event -> {
             setEffect(drop);
             setCursor(Cursor.HAND);
@@ -40,14 +43,18 @@ class HousesView extends ImageView
             setEffect(null);
             setCursor(null);
         });
+        //設定點下滑鼠的事件
+        setOnMouseClicked(event -> fireEvent(move));
         setImage(chessImage);
         setChessNum(initial);
     }
+    //設定棋洞內的旗子數目
     public void setChessNum(int chessNum)
     {
         setViewport(clips[chessNum]);
         this.chessNum = chessNum;
     }
+    //取得棋洞位置
     public int[] getBoardPosition()
     {
         int pos[] = {boardSide, boardNum};
@@ -56,18 +63,63 @@ class HousesView extends ImageView
 }
 class BoardPane extends GridPane
 {
-    private HousesView[] houses = new HousesView[12];
+    private HousesView[][] houses = new HousesView[2][6];
     private Board oware;
 
     public BoardPane(Board oware, Image chessImage)
     {
         super();
         this.oware = oware;
+        //初始化棋洞，並將棋洞放入棋盤的格子中
         for(int i=0; i < houses.length; i++)
         {
-            houses[i] = new HousesView(chessImage, 25, 5, 5, (i/6), (i%6), oware.getHouses((i/6), (i%6)));
-            add(houses[i], (i%6), (i/6));
+            for(int j=0; j < houses[i].length; j++)
+            {
+                houses[i][j] = new HousesView(chessImage, 25, 5, 5, i, j, oware.getHouses(i, j));
+                add(houses[i][j], j, i);
+            }
         }
+        //建立事件接收器，當接受到棋洞被點擊傳來的事件時，呼叫HousesMove()進行移動
+        this.addEventHandler(HousesMoveEvent.HOUSES_MOVE, event -> HousesMove(event.getPosX(), event.getPosY()));
+    }
+    //直接調用Board的move方法，並用updateBoard()刷新棋盤
+    public void HousesMove(int startSide, int startNum)
+    {
+        try
+        {
+            oware.move(startSide, startNum);
+            updateBoard();
+        }
+        catch(InvalidMoveException e)
+        {
+        }
+    }
+    //刷新棋盤的函數
+    public void updateBoard()
+    {
+        for(int i=0; i < houses.length; i++)
+        {
+            for(int j=0; j < houses[i].length; j++)
+                houses[i][j].setChessNum(oware.getHouses(i, j));
+        }
+    }
+}
+class HousesMoveEvent extends Event
+{
+    private int[] pos;
+    public static final EventType<HousesMoveEvent> HOUSES_MOVE = new EventType<>(Event.ANY, "HOUSES_MOVE");
+    public HousesMoveEvent(int[] pos)
+    {
+        super(HOUSES_MOVE);
+        this.pos = pos;
+    }
+    public int getPosX()
+    {
+        return pos[0];
+    }
+    public int getPosY()
+    {
+        return pos[1];
     }
 }
 public class OwareBoardGUI extends Application
